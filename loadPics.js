7a022e9f15780eade5a195a9dc94c1bcda6e4475
CaptainGobelin <captaingobelin@gmail.com>
2014-03-27 09:18:48 +0100

    You can now change an image.

--- loadPics.js
+++ loadPics.js
@@ -1,8 +1,9 @@
 var ID = (function() {
-     var private = {
-         '1': 'yak',
-     };
-     return {
+    var private = {
+        '0' : 'No_sign',
+        '1': 'yak',
+    };
+    return {
         get: function(name) { return private[name]; }
     };
 })();
@@ -15,12 +16,38 @@ $.wordWithPics  = function() {
     	$picsDiv = $("div.picsDiv");
         $picsDiv.empty();
     	for (var i=0;i<id.length;i++) {
+            var picId = 0;
     		if ((id.charAt(i) < 2) && (id.charAt(i) > 0))
-    			$s = '<embed id="'+ID.get(id.charAt(i))+i+'" src="pics/' + id.charAt(i);
-    		else
-    			$s = '<embed id="notFound'+i+'" src="pics/No_sign';
-    		$s += '.svg" type="image/svg+xml" height="100px" width="100px">';
+                picId = id.charAt(i);
+    		$s = '<img id="'+ID.get(picId)+i+'" src="pics/' + picId;
+    		$s += '.svg" height="100px" width="100px" onClick="$.showPicsList('+picId+','+i+')">';
     		$picsDiv.append($s);
     	}
     }
 };
+
+$.showPicsList = function(id, num) {
+    $listDiv = $("div.listDiv");
+    $listDiv.empty();
+    $s = "Images disponibles: ";
+    for (var i=1;i<2;i++) {
+        $s += '<img id="'+ID.get(i)+'_list " src="pics/' + i;
+        $s += '.svg" type="image/svg+xml" height="100px" width="100px"';
+        $s += 'onClick="$.changePic('+id+','+i+','+num+')">';
+    }
+    $listDiv.append($s);
+}
+
+$.changePic = function(oldId, image, num) {
+    var newId = '';
+    newId += ID.get(image) + num;
+    var newSrc = 'pics/' + image + '.svg';
+    document.getElementById(ID.get(oldId)+num).src = newSrc;
+    document.getElementById(ID.get(oldId)+num).id = newId;
+    $.hidePicsList();
+}
+
+$.hidePicsList = function() {
+    $listDiv = $("div.listDiv");
+    $listDiv.empty();
+}

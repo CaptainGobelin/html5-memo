efd72eadc82dea6ae4fb3c6147932cbae8e8898f
CaptainGobelin <captaingobelin@gmail.com>
2014-06-03 10:27:01 +0200

    Fix timer

--- js/timer.js
+++
@@ -1,8 +0,0 @@
-$.startTimer = function() {
-	window.setInterval(function() {
-	if ($.getCookie("stopTimer") == false) {
-		$.deleteCookies();
-		document.location = 'index.html';
-	}
-	}, 10000);
-}
